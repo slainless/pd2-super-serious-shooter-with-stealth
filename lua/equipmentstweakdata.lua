3ff981401a46522fdd3effd073c99a3ce014e233
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-07-15 08:39:30 +0800

    fix: disable default equipment tweaks since partial skill support was added

--- lua/equipmentstweakdata.lua
+++ lua/equipmentstweakdata.lua
@@ -1,6 +1,6 @@
-Hooks:PostHook(EquipmentsTweakData, "init", "init_sss", function (self)
-	self.trip_mine.quantity = { 4, 4 }
-	self.ecm_jammer.quantity = { 2 }
-	self.first_aid_kit.quantity = { 6 }
-	self.specials.cable_tie.quantity = 9
-end)
+-- Hooks:PostHook(EquipmentsTweakData, "init", "init_sss", function (self)
+-- 	self.trip_mine.quantity = { 4, 4 }
+-- 	self.ecm_jammer.quantity = { 2 }
+-- 	self.first_aid_kit.quantity = { 6 }
+-- 	self.specials.cable_tie.quantity = 9
+-- end)
